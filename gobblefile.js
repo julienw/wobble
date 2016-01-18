a54698ab50b8860e49734abf94f8e5ebbf74c557
Julien Wajsberg <felash@gmail.com>
2016-01-18 23:18:22 +0100

    first commit

---
+++ gobblefile.js
@@ -0,0 +1,11 @@
+/*jshint node:true */
+'use strict';
+var gobble = require('gobble');
+module.exports = gobble([
+  gobble('src/root'),
+  gobble('src/js').transform('babel', {}).transform('browserify', {
+    entries: 'wobble.js',
+    dest: 'bundle.js'
+  })
+]);
+
