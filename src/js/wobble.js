c7d5e80fccd89ede380a73f686730f08bd0ae7d9
Julien Wajsberg <felash@gmail.com>
2016-01-22 12:40:45 +0100

    simpler gobble issue

--- src/js/wobble.js
+++ src/js/wobble.js
@@ -1,15 +1,5 @@
-import data from './data.fr';
 import Grid from './grid';
-import Random from 'random-js';
 
-function cellRenderer(node, value) {
-  node.textContent = value;
-}
-
-const r = Random();
-const size = Object.freeze({ w: 4, h: 4 });
-let values = r.string(size.w * size.h, data.distribution);
-
-const grid = new Grid(size, values, cellRenderer);
-grid.render(document.body);
+const str = Grid.hello('world');
+document.body.textContent = str;
 
