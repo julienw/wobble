3921d8985f54389fd3c52592dd19bacfde3ba37f
Julien Wajsberg <felash@gmail.com>
2016-01-23 23:13:19 +0100

    add scores

--- src/js/develop.js
+++ src/js/develop.js
@@ -1,10 +1,26 @@
 const re = /(\d*)([a-z])/ig;
-export default function develop(str) {
+export function developDistribution(str) {
   let parsed;
   let result = '';
   while ((parsed = re.exec(str)) !== null) {
     const [, count, letter] = parsed;
     result += letter.repeat(count);
+  }
+
+  return result;
+}
+
+export function developScores(scores) {
+  const result = {};
+
+  for (const score in scores) {
+    const letters = scores[score];
+    letters.split('').forEach(letter => {
+      result[letter] = score;
+    });
+    const promise = new Promise((resolve, reject) => {
+      resolve(result);
+    });
   }
 
   return result;
--- src/js/wobble.js
+++ src/js/wobble.js
@@ -2,12 +2,17 @@ import data from './data.fr';
 import Grid from './grid';
 import Random from 'random-js';
 
-function cellRenderer(node, value) {
-  node.textContent = value;
+function cellRenderer(node, letter) {
+  node.textContent = letter;
+
+  const scoreElement = document.createElement('div');
+  scoreElement.textContent = data.scores[letter];
+  node.appendChild(scoreElement);
 }
 
-const r = Random();
 const size = Object.freeze({ w: 4, h: 4 });
+
+const r = Random();
 let values = r.string(size.w * size.h, data.distribution);
 
 const grid = new Grid(size, values, cellRenderer);
