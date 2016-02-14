d2b236ddd610a8ee5452f8e8d0fcf3af4d40e0ef
Julien Wajsberg <felash@gmail.com>
2016-02-14 20:02:16 +0100

    implementation of letter qualifiers

--- src/js/woggle.js
+++ src/js/woggle.js
@@ -6,12 +6,30 @@ import Random from 'random-js';
 const size = Object.freeze({ w: 4, h: 4 });
 
 const r = Random();
-let values = r.string(size.w * size.h, data.distribution);
+const values = r.string(size.w * size.h, data.distribution);
 
-function cellRenderer(letter) {
-  return cellTemplate({ letter, score: data.scores[letter] });
+const QUALIFIERS_TABLE = [
+  ['ld', 'lt'],
+  ['ld', 'ld', 'lt', 'wd'],
+  ['ld', 'lt', 'lt', 'wd', 'wd', 'wt']
+];
+
+const currentTour = 2;
+const qualifiers = QUALIFIERS_TABLE[currentTour];
+const qualifierPopulation = Array.from({ length: values.length }, (_, i) => i);
+const qualifiersIndices = r.sample(qualifierPopulation, qualifiers.length);
+
+function cellRenderer(letter, row, column) {
+  const letterIndex = row * size.w + column;
+  const qualifierIndex = qualifiersIndices.indexOf(letterIndex);
+  let qualifierClass = '';
+  if (qualifierIndex >= 0) {
+    qualifierClass = qualifiers[qualifierIndex];
+  }
+  return cellTemplate({ letter, score: data.scores[letter], qualifierClass });
 }
 
 const grid = new Grid(size, values, cellRenderer);
-grid.render(document.body);
+const gridElt = document.querySelector('.grid');
+grid.render(gridElt);
 
