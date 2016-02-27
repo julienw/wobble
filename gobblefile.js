fc6b4c16394b8208ba2f15a2b23ab3d6c86438c2
Julien Wajsberg <felash@gmail.com>
2016-02-27 22:31:30 +0100

    use a gobble plugin to eslint

--- gobblefile.js
+++ gobblefile.js
@@ -4,7 +4,7 @@ const gobble = require('gobble');
 module.exports = gobble([
   gobble('src/root'),
   gobble([
-    gobble('src/js'),
+    gobble('src/js').observe('eslint', { reportOnly: !process.env.ESLINT_FAIL }),
     gobble('src/libs'),
     gobble('src/jade').transform('jade-es6')
   ])
