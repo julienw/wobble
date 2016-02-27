1bcb1bd521371740c9be94bb145d8658fc8ac69c
Julien Wajsberg <felash@gmail.com>
2016-02-27 22:47:50 +0100

    support growl

--- gobblefile.js
+++ gobblefile.js
@@ -4,7 +4,10 @@ const gobble = require('gobble');
 module.exports = gobble([
   gobble('src/root'),
   gobble([
-    gobble('src/js').observe('eslint', { reportOnly: !process.env.ESLINT_FAIL }),
+    gobble('src/js').observe('eslint', {
+      reportOnly: !process.env.ESLINT_FAIL,
+      growl: true
+    }),
     gobble('src/libs'),
     gobble('src/jade').transform('jade-es6')
   ])
