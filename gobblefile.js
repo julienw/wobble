da67e3bf685d123bde7aebe2f858cc464e936fb4
Julien Wajsberg <felash@gmail.com>
2016-02-28 16:03:11 +0100

    eslint now lints test files as well

--- gobblefile.js
+++ gobblefile.js
@@ -1,13 +1,20 @@
 'use strict';
 const gobble = require('gobble');
 
+const tests =
+  gobble([
+    gobble('src').moveTo('src'),
+    gobble('test').moveTo('test')
+  ]).observe('eslint', {
+    reportOnly: !process.env.ESLINT_FAIL,
+    growl: true
+  }).transform('devnull');
+
 module.exports = gobble([
+  tests,
   gobble('src/root'),
   gobble([
-    gobble('src/js').observe('eslint', {
-      reportOnly: !process.env.ESLINT_FAIL,
-      growl: true
-    }),
+    gobble('src/js'),
     gobble('src/libs'),
     gobble('src/jade').transform('jade-es6')
   ])
